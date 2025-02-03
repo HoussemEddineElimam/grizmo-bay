955fc18312ef20b9a4ed1e6689255fed29f9aea2
HoussemEddineElimam <houssemelimam.univ@gmail.com>
2025-02-03 12:22:22 +0100

    updated

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class ProductDetailPage extends Component
-{
+{   
+    public $slug;
+    public function mount($slug){
+        return $this->slug = $slug;
+    }
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page',["product"=>Product::query()->where('slug',$this->slug)->firstOrFail()]);
     }
 }
--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductsPage extends Component
-{
+{   
+    use WithPagination;
     public function render()
-    {
-        return view('livewire.products-page');
+    {   
+        $products = Product::query()->where('is_active',1);
+        $brands = Brand::where('is_active',1)->get(['id','name','slug']);
+        $categories = Category::where('is_active',1)->get(['id','name','slug']);
+        return view('livewire.products-page',['products'=>$products->paginate(9),'brands'=>$brands,'categories'=>$categories]);
     }
 }
